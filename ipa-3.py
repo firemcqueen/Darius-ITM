5e035331d317665e0e4217f0abcfb316e9f142b7
Darius Cruz <dariuscruz@Dars-MacBook.local>
2023-07-04 01:26:11 +0800

    advanced programming

---
+++ ipa-3.py
@@ -0,0 +1,302 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+def relationship_status(from_member, to_member, social_graph):
+    '''Relationship Status.
+    20 points.
+
+    Let us pretend that you are building a new app.
+    Your app supports social media functionality, which means that users can have
+    relationships with other users.
+
+    There are two guidelines for describing relationships on this social media app:
+    1. Any user can follow any other user.
+    2. If two users follow each other, they are considered friends.
+
+    This function describes the relationship that two users have with each other.
+
+    Please see "assignment-4-sample-data.py" for sample data. The social graph
+    will adhere to the same pattern.
+
+    Parameters
+    ----------
+    from_member: str
+        the subject member
+    to_member: str
+        the object member
+    social_graph: dict
+        the relationship data
+
+    Returns
+    -------
+    str
+        "follower" if fromMember follows toMember,
+        "followed by" if fromMember is followed by toMember,
+        "friends" if fromMember and toMember follow each other,
+        "no relationship" if neither fromMember nor toMember follow each other.
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    
+    following_from_member = social_graph.get(from_member, {}).get("following", [])
+    following_to_member = social_graph.get(to_member, {}).get("following", [])
+
+    if to_member in following_from_member:
+        if from_member in following_to_member:
+            return "friends"
+        else:
+            return "follower"
+    elif from_member in following_to_member:
+        return "followed by"
+    else:
+        return "no relationship"
+
+
+# In[25]:
+
+
+social_graph = {
+    "@bongolpoc":{"first_name":"Joselito",
+                  "last_name":"Olpoc",
+                  "following":[
+                  ]
+    },
+    "@joaquin":  {"first_name":"Joaquin",
+                  "last_name":"Gonzales",
+                  "following":[
+                      "@chums","@jobenilagan"
+                  ]
+    },
+    "@chums" : {"first_name":"Matthew",
+                "last_name":"Uy",
+                "following":[
+                    "@bongolpoc","@miketan","@rudyang","@joeilagan"
+                ]
+    },
+    "@jobenilagan":{"first_name":"Joben",
+                   "last_name":"Ilagan",
+                   "following":[
+                    "@eeebeee","@joeilagan","@chums","@joaquin"
+                   ]
+    },
+    "@joeilagan":{"first_name":"Joe",
+                  "last_name":"Ilagan",
+                  "following":[
+                    "@eeebeee","@jobenilagan","@chums"
+                  ]
+    },
+    "@eeebeee":  {"first_name":"Elizabeth",
+                  "last_name":"Ilagan",
+                  "following":[
+                    "@jobenilagan","@joeilagan"
+                  ]
+    },
+}
+
+relationship_status("@joaquin", "@jobenilagan", social_graph)
+
+
+# In[26]:
+
+
+def tic_tac_toe(board):
+    '''Tic Tac Toe.
+    25 points.
+
+    Tic Tac Toe is a common paper-and-pencil game.
+    Players must attempt to successfully draw a straight line of their symbol across a grid.
+    The player that does this first is considered the winner.
+
+    This function evaluates a tic tac toe board and returns the winner.
+
+    Please see "assignment-4-sample-data.py" for sample data. The board will adhere
+    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
+    have more than one winner. The board will only ever have 2 unique symbols at the same time.
+
+    Parameters
+    ----------
+    board: list
+        the representation of the tic-tac-toe board as a square list of lists
+
+    Returns
+    -------
+    str
+        the symbol of the winner or "NO WINNER" if there is no winner
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    
+    size = len(board)
+
+    # Check rows
+    for row in board:
+        if len(set(row)) == 1 and row[0] != '':
+            return row[0]
+
+    # Check columns
+    for col in range(size):
+        column = [board[row][col] for row in range(size)]
+        if len(set(column)) == 1 and column[0] != '':
+            return column[0]
+
+    # Check diagonals
+    diagonal1 = [board[i][i] for i in range(size)]
+    diagonal2 = [board[i][size - 1 - i] for i in range(size)]
+    if len(set(diagonal1)) == 1 and diagonal1[0] != '':
+        return diagonal1[0]
+    if len(set(diagonal2)) == 1 and diagonal2[0] != '':
+        return diagonal2[0]
+
+    return "NO WINNER"
+
+
+# In[30]:
+
+
+board1 = [
+['X','X','O'],
+['O','X','O'],
+['O','','X'],
+]
+
+board2 = [
+['X','X','O'],
+['O','X','O'],
+['','O','X'],
+]
+
+board3 = [
+['O','X','O'],
+['','O','X'],
+['X','X','O'],
+]
+
+board4 = [
+['X','X','X'],
+['O','X','O'],
+['O','','O'],
+]
+
+board5 = [
+['X','X','O'],
+['O','X','O'],
+['X','','O'],
+]
+
+board6 = [
+['X','X','O'],
+['O','X','O'],
+['X','',''],
+]
+
+board7 = [
+['X','X','O',''],
+['O','X','O','O'],
+['X','','','O'],
+['O','X','','']
+]
+
+tic_tac_toe(board1)
+
+
+# In[43]:
+
+
+def eta(first_stop, second_stop, route_map):
+    '''ETA.
+    25 points.
+
+    A shuttle van service is tasked to travel along a predefined circlar route.
+    This route is divided into several legs between stops.
+    The route is one-way only, and it is fully connected to itself.
+
+    This function returns how long it will take the shuttle to arrive at a stop
+    after leaving another stop.
+
+    Please see "mod-4-ipa-1-sample-data.py" for sample data. The route map will
+    adhere to the same pattern. The route map may contain more legs and more stops,
+    but it will always be one-way and fully enclosed.
+
+    Parameters
+    ----------
+    first_stop: str
+        the stop that the shuttle will leave
+    second_stop: str
+        the stop that the shuttle will arrive at
+    route_map: dict
+        the data describing the routes
+
+    Returns
+    -------
+    int
+        the time it will take the shuttle to travel from first_stop to second_stop
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    
+    current_stop = first_stop
+    total_travel_time = 0
+
+    while current_stop != second_stop:
+        try:
+            leg = route_map[(current_stop, second_stop)]
+            travel_time = leg['travel_time_mins']
+            total_travel_time += travel_time
+            current_stop = second_stop
+        except KeyError:
+            # Handle if there is no direct leg from current_stop to second_stop
+            next_stop_found = False
+
+            for stop_pair, leg in route_map.items():
+                if stop_pair[0] == current_stop:
+                    next_stop = stop_pair[1]
+                    travel_time = leg['travel_time_mins']
+                    total_travel_time += travel_time
+                    current_stop = next_stop
+                    next_stop_found = True
+                    break
+
+            if not next_stop_found:
+                return -1  # Indicate that there is no valid route
+
+    return total_travel_time
+
+
+# In[50]:
+
+
+legs = {
+     ("upd","admu"):{
+         "travel_time_mins":10
+     },
+     ("admu","dlsu"):{
+         "travel_time_mins":35
+     },
+     ("dlsu","upd"):{
+         "travel_time_mins":55
+     }
+}
+
+arms = {
+    ('a1', 'a2'): {
+        'travel_time_mins': 10
+    },
+    ('a2', 'b1'): {
+        'travel_time_mins': 10230
+    },
+    ('b1', 'a1'): {
+        'travel_time_mins': 1
+    }
+}
+
+eta("admu", "dlsu", legs)
+
+
+# In[ ]:
+
+
+
+
